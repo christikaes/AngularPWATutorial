d49d72138f3b07805d964bb61e76e55652caaa8f
christikaes <christikaes@gmail.com>
2018-07-19 10:34:45 -0400

    Part1-InitialAngularApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  characterNames = [];
+  characters = [];
+
+  ngOnInit() {
+    // Load the data.json and populate characterNames and data
+    this.loadData();
+  }
+
+  loadData = async () => {
+    const response = await fetch('assets/data.json');
+    const data = await response.json();
+    this.characters = data.characters;
+    this.characterNames = Object.keys(data.characters);
+  }
+
+  playSound = async (characterName) => {
+    const characterSounds = this.characters[characterName];
+    const soundName = characterSounds[Math.floor(Math.random() * characterSounds.length)];
+    const audio = new Audio('assets/sound/' + soundName + '.mp3');
+    audio.play();
+  }
 }
